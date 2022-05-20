99036002b43fcfc4f165570839f235a468e9c7e7
Maxim_Kalyadin <kalyadin.maxim@gmail.com>
2022-05-20 12:14:37 +0400

    first commit

---
+++ ConsoleApp1/Hashcs.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Hashcs
+    {
+        private int size = 255;
+        private int step = 10;
+        Dictionary<int, string> hashmap;
+
+        public Hashcs()
+        {
+            hashmap = new Dictionary<int, string>(size);
+        }
+
+        public int F(int key)
+        {
+            return key % size;
+        }
+
+        public void Add(int key, string value)
+        {
+            int hash = F(key);
+            for (int i = 0; i < size; i++)
+            {
+                int hashStep = hash + (i * step);
+                if (hashStep > size)
+                {
+                    break;
+                }
+                if (!hashmap.ContainsKey(hashStep))
+                {
+                    hashmap.Add(hashStep, value);
+                    break;
+                }
+            }
+            
+        }
+
+        public string search(int key)
+        {
+            int hash = F(key);
+            for (int i = 0; i < size; i++)
+            {
+                int hashStep = hash + (i * step);
+                if (hashStep > size)
+                {
+                    return null;
+                }
+                if (hashmap.ContainsKey(hashStep))
+                {
+                    return hashmap[hashStep];
+                }
+            }
+            return null;
+        }
+        // https://pikabu.ru/story/kheshtablitsa_hashtable_na_yazyike_c_5805617
+        // https://razilov-code.ru/2018/11/02/binarytree-on-csharp-and-java/
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree<int> tree = new Tree<int>(3, null);
+            tree.Add(5);
+            tree.Add(3);
+            tree.Add(1);
+            tree.Add(4);
+            Tree<int> t = tree.serch(3);
+            t = tree.serch(1);
+            t = tree.serch(4);
+            tree.print();
+        }
+    }
+}
